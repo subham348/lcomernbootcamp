957d38026e46d98716a4745e1d1734fa055a7349
subham348 <subhammahato348@gmail.com>
2020-11-04 22:14:47 +0530

    On folder models, added three files category.js product.js order.js

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -44,7 +44,9 @@ import { v1 as uuidv1 } from 'uuid';
         type: Array,
         default: []
     }
-  });
+  },
+  {timestamps: true}
+  );
 
   userSchema.virtual("password")
     .set(function(password) {
