9d2acdf7a1e0409ce484f7b104ff898ffa8d41e1
subham348 <subhammahato348@gmail.com>
2020-11-24 00:53:21 +0530

    Used populate in userPurchaseList and also included a middleware pushOrderInPurchaseList

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Order = require("../models/order");
 
 exports.getUserById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -35,6 +36,51 @@ exports.updateUser = (req, res) => {
             user.salt = undefined;
             user.encry_password = undefined;
             res.json(user);
+        }
+    );
+};
+
+
+exports.userPurchaseList = (req, res) => {
+    Order.find({user: req.profile._id})
+    .populate("user", "_id name")
+    .exec((err, order) => {
+        if(err) {
+            return res.status(400).json({
+                error: "No Order in this account"
+            });
+        }
+        return res.json(order);
+    });
+};
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+
+    let purchases = [];
+    req.body.order.products.array.forEach(product => {
+        purchases.push({
+            id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transacton_id: req.body.order.transacton_id
+        });
+    });
+
+    // store this in DB
+    User.findOneAndUpdate(
+        {_id: req.profile._id},
+        {$push: {purchases: purchases}},
+        {new: true},
+        (err, purchases) => {
+            if(err) {
+                return res.status(400).json({
+                    error: "Unable to save purchase list" 
+                })
+            }
+            next();
         }
     );
 };
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getUserById, getUser, updateUser} = require("../controllers/user");
+const {getUserById, getUser, updateUser, userPurchaseList} = require("../controllers/user");
 // const {getUserById, getUser, getAllUsers} = require("../controllers/user");
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 
@@ -10,6 +10,9 @@ router.param("userId", getUserById);
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+
+// populating
+router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList);
 
 // getAllUsers was an assignment to get all users at once
 // router.get("/users", getAllUsers);
